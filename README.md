53f54ba51e493857c7b3fdc5ec0ee98db22e6269
g-barbosa <giovanne.bbarbosa@gmail.com>
2021-03-29 17:55:37 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# My Personal Typescript project files structure
---
+++ jest.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  clearMocks: true,
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/**/*-protocols.ts',
+    '!**/protocols/**',
+    '!**/controller/**',
+    '!<rootDir>/src/**/*I[A-Z]*.ts'
+  ],
+  coveragePathIgnorePatterns: [
+    '<rootDir>/src/I[A-Z]*.ts'
+  ],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
